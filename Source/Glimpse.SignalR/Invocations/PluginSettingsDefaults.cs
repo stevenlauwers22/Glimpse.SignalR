d6a48859289b9f86bf127823838ae58245a99961
Umair Ishaq <umair29@gmail.com>
2014-07-15 10:53:46 -0400

    Added locking on the Invocations collection

--- Source/Glimpse.SignalR/Invocations/PluginSettingsDefaults.cs
+++ Source/Glimpse.SignalR/Invocations/PluginSettingsDefaults.cs
@@ -1,21 +1,68 @@
-using System;
-using System.Collections.Generic;
-using Glimpse.SignalR.Invocations.Plumbing;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PluginSettingsDefaults.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The plugin settings defaults.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Glimpse.SignalR.Invocations
 {
+    using System;
+    using System.Collections.Generic;
+
+    using Glimpse.SignalR.Invocations.Plumbing;
+
+    /// <summary>
+    /// The plugin settings defaults.
+    /// </summary>
     public static class PluginSettingsDefaults
     {
-        private static readonly ICollection<InvocationModel> Invocations = new List<InvocationModel>(); 
-        
+        #region Static Fields
+
+        /// <summary>
+        /// The invocations.
+        /// </summary>
+        private static readonly ICollection<InvocationModel> Invocations = new List<InvocationModel>();
+
+        /// <summary>
+        /// The locker.
+        /// </summary>
+        private static object locker = new object();
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The get invocations func.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Action"/>.
+        /// </returns>
         public static Func<IEnumerable<InvocationModel>> GetInvocationsFunc()
         {
             return () => Invocations;
         }
 
+        /// <summary>
+        /// The store invocation.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Action"/>.
+        /// </returns>
         public static Action<InvocationModel> StoreInvocation()
         {
-            return invocation => Invocations.Add(invocation);
+            return invocation =>
+                {
+                    lock (locker)
+                    {
+                        Invocations.Add(invocation);
+                    }
+                };
         }
+
+        #endregion
     }
 }
