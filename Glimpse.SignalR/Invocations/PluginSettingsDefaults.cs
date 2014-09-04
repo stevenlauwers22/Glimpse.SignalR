62a231ef5d15d0e6881a0c944bc3427cdcae2ab0
stevenlauwers22 <stevenlauwers22@hotmail.com>
2014-09-04 08:52:23 +0200

    Removed regions and useless comments

--- Glimpse.SignalR/Invocations/PluginSettingsDefaults.cs
+++ Glimpse.SignalR/Invocations/PluginSettingsDefaults.cs
@@ -1,68 +1,28 @@
-// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="PluginSettingsDefaults.cs" company="">
-//   
-// </copyright>
-// <summary>
-//   The plugin settings defaults.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using Glimpse.SignalR.Invocations.Plumbing;
 
 namespace Glimpse.SignalR.Invocations
 {
-    using System;
-    using System.Collections.Generic;
-
-    using Glimpse.SignalR.Invocations.Plumbing;
-
-    /// <summary>
-    /// The plugin settings defaults.
-    /// </summary>
     public static class PluginSettingsDefaults
     {
-        #region Static Fields
-
-        /// <summary>
-        /// The invocations.
-        /// </summary>
         private static readonly ICollection<InvocationModel> Invocations = new List<InvocationModel>();
+        private static readonly object Locker = new object();
 
-        /// <summary>
-        /// The locker.
-        /// </summary>
-        private static object locker = new object();
-
-        #endregion
-
-        #region Public Methods and Operators
-
-        /// <summary>
-        /// The get invocations func.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="Action"/>.
-        /// </returns>
         public static Func<IEnumerable<InvocationModel>> GetInvocationsFunc()
         {
             return () => Invocations;
         }
 
-        /// <summary>
-        /// The store invocation.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="Action"/>.
-        /// </returns>
         public static Action<InvocationModel> StoreInvocation()
         {
             return invocation =>
                 {
-                    lock (locker)
+                    lock (Locker)
                     {
                         Invocations.Add(invocation);
                     }
                 };
         }
-
-        #endregion
     }
 }
